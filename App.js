7fd511224f2adcfbc9d9b7f28d2d34a31fc8e8e3
arun <arunsolanki301@gmail.com>
2023-08-11 15:07:09 +0530

    added splashScreen

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import {StyleSheet, View, Animated, Easing} from 'react-native';
+import Lottie from 'lottie-react-native';
 
-import Home from './src/screens/Home';
 import {NavigationContainer} from '@react-navigation/native';
 import {store} from './src/redux/store';
 import {Provider} from 'react-redux';
 import MyStack from './src/navigator/Stack';
+import AnimatedLottieView from 'lottie-react-native';
 
 const App = () => {
+  const [animationIsVisible, setAnimationIsVisible] = useState(true);
+  const ref = useRef(null);
+  const opacity = useRef(new Animated.Value(1));
+  const progress = useRef(new Animated.Value(0));
+  useEffect(() => {
+    const unsubscribe = setTimeout(() => {
+      if (!progress.current) {
+        return null;
+      }
+
+      Animated.sequence([
+        Animated.timing(progress.current, {
+          toValue: 1,
+          useNativeDriver: true,
+          duration: 2000,
+          easing: Easing.ease,
+        }),
+        Animated.timing(opacity.current, {
+          delay: 250,
+          toValue: 0,
+          useNativeDriver: true,
+          duration: 250,
+          easing: Easing.in(Easing.ease),
+        }),
+      ]).start(() => {
+        setAnimationIsVisible(false);
+      });
+    }, 500);
+    return () => {
+      clearTimeout(unsubscribe);
+    };
+  }, []);
   return (
-    <NavigationContainer>
-      <Provider store={store}>
-        <View style={styles.container}>
-          <MyStack />
-        </View>
-      </Provider>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Provider store={store}>
+          <View style={styles.container}>
+            <MyStack />
+          </View>
+        </Provider>
+      </NavigationContainer>
+      {animationIsVisible && (
+        <Animated.View
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              backgroundColor: '#FFFFFF',
+              alignItems: 'center',
+              justifyContent: 'center',
+              opacity: opacity.current,
+            },
+          ]}>
+          <Lottie
+            ref={ref}
+            source={require('./src/image/splash01.json')}
+            loop={false}
+            progress={progress.current}
+            resizeMode="cover"
+          />
+        </Animated.View>
+      )}
+    </>
   );
 };
 
--- src/navigator/Stack.js
+++ src/navigator/Stack.js
@@ -4,12 +4,21 @@ import React from 'react';
 import Login from '../screens/Login';
 import SignUp from '../screens/SignUp';
 import Home from '../screens/Home';
+// import Splash from '../screens/Splash';
 const Stack = createStackNavigator();
 export default function MyStack() {
   return (
     <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="SignUp"
+        component={SignUp}
+        options={{headerShown: false}}
+      />
       <Stack.Screen name="Home" component={Home} />
     </Stack.Navigator>
   );
