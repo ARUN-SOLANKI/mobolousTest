80b7628f1a404be2aa43d2cbf1cb30904943c674
arun solanki <arunsolanki301@gmail.com>
2023-08-23 12:13:21 +0530

    test completed

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,9 +1,48 @@
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { Alert, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import Button from '../Button';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Card = ({item}) => {
-  console.log(item, '++++++++');
+const Card = ({ item, setInventories }) => {
+
+  const handleRemove = async (id) => {
+    const user = await AsyncStorage.getItem('user');
+    if (user.roles === "dm") {
+      Alert.alert("You are not able to change the status. For that You have to change your role")
+      return
+    }
+    const inventories = await JSON.parse(
+      await AsyncStorage.getItem('inventories'),
+    );
+    const updatedInventories = inventories.filter((item) => item.product_id !== id)
+    if (updatedInventories.length) {
+      await AsyncStorage.setItem('inventories', JSON.stringify(updatedInventories));
+      setInventories(updatedInventories)
+    }
+  }
+
+  const handleAprove = async (id) => {
+    const user = await AsyncStorage.getItem('user');
+    if (user.roles === "dm") {
+      Alert.alert("You are not able to change the status. For that You have to change your role")
+      return
+    }
+    const inventories = await JSON.parse(
+      await AsyncStorage.getItem('inventories'),
+    );
+    const updatedInventories = inventories.map((item) => {
+      if (item.product_id === id) {
+        return { ...item, status: "Approved" }
+      } else {
+        return item;
+      }
+    })
+    if (updatedInventories.length) {
+      await AsyncStorage.setItem('inventories', JSON.stringify(updatedInventories));
+      setInventories(updatedInventories)
+    }
+  }
+
   return (
     <TouchableOpacity style={styles.cardContainer}>
       <Text style={styles.cardTitle}>batch_date : {item.batch_date}</Text>
@@ -14,8 +53,8 @@ const Card = ({item}) => {
       <Text style={styles.cardTitle}>vendor : {item.vendor}</Text>
       <Text style={styles.cardTitle}>Status : {item.status}</Text>
       <View style={styles.buttonContainer}>
-        {item.status == 'Pending' && <Button text="approve" />}
-        <Button text="remove" />
+        {item.status == 'Pending' && <Button text="approve" onPress={() => handleAprove(item.product_id)} />}
+        <Button text="remove" onPress={() => handleRemove(item.product_id)} />
       </View>
     </TouchableOpacity>
   );
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -4,17 +4,30 @@ import {
   Text,
   View,
   FlatList,
+  Modal,
 } from 'react-native';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import data from '../../utils/jsonData';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Card from '../../components/Card';
 import Button from '../../components/Button';
+import { TextInput } from 'react-native-gesture-handler';
 
 const Home = () => {
   const [loader, setLoader] = useState(false);
   const [user, setUser] = useState({});
   const [inventories, setInventories] = useState([]);
+  const [isVisible, setVisible] = useState(true);
+  const [productData, setProductData] = useState({
+    product_id: '',
+    product_name: '',
+    vendor: '',
+    mrp: '',
+    batch_num: '',
+    batch_date: '',
+    quantity: '',
+    status: ''
+  });
   useEffect(() => {
     const getInventories = async () => {
       const invent = await JSON.parse(
@@ -28,24 +41,120 @@ const Home = () => {
 
   if (loader) {
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <ActivityIndicator />
       </View>
     );
   }
 
+  const handleInputChange = (field, value) => {
+    setProductData(prevData => ({
+      ...prevData,
+      [field]: value
+    }));
+  };
+
+  const handleSubmit = async () => {
+    await AsyncStorage.setItem('inventories', JSON.stringify([...inventories, productData]));
+    setInventories([...inventories, productData])
+    setProductData({
+      product_id: '',
+      product_name: '',
+      vendor: '',
+      mrp: '',
+      batch_num: '',
+      batch_date: '',
+      quantity: '',
+      status: ''
+    });
+    onClose();
+  };
+
+  const onClose = () => {
+    setVisible(false)
+  }
+
   return (
     <View>
       <View style={styles.addInvent}>
-        <Button text="Add Inventories" style={{width: '50%'}} />
+        <Button text="Add Inventories" style={{ width: '50%' }} onPress={() => setVisible(true)} />
       </View>
       <FlatList
         data={inventories}
-        style={{marginBottom: 100}}
-        renderItem={({item}) => {
-          return <Card item={item} />;
+        style={{ marginBottom: 100 }}
+        renderItem={({ item }) => {
+          return <Card item={item} setInventories={setInventories} />;
         }}
       />
+      <Modal
+        visible={isVisible}
+        animationType="slide"
+        transparent={true}
+      >
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+          <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10, width: '80%' }}>
+            <TextInput
+              placeholder="Product ID"
+              placeholderTextColor="gray"
+              value={productData.product_id}
+              onChangeText={text => handleInputChange('product_id', text)}
+              style={styles.inputField}
+            />
+            <TextInput
+              placeholder="Product Name"
+              placeholderTextColor="gray"
+              value={productData.product_name}
+              onChangeText={text => handleInputChange('product_name', text)}
+              style={styles.inputField}
+            />
+            <TextInput
+              placeholder="Vendor"
+              placeholderTextColor="gray"
+              value={productData.vendor}
+              onChangeText={text => handleInputChange('vendor', text)}
+              style={styles.inputField}
+            />
+            <TextInput
+              placeholder="MRP"
+              placeholderTextColor="gray"
+              value={productData.mrp}
+              onChangeText={text => handleInputChange('mrp', text)}
+              style={styles.inputField}
+            />
+            <TextInput
+              placeholder="BATCH NUM"
+              placeholderTextColor="gray"
+              value={productData.batch_num}
+              onChangeText={text => handleInputChange('batch_num', text)}
+              style={styles.inputField}
+            />
+            <TextInput
+              placeholder="BATCH DATE"
+              placeholderTextColor="gray"
+              value={productData.batch_date}
+              onChangeText={text => handleInputChange('batch_date', text)}
+              style={styles.inputField}
+            />
+            <TextInput
+              placeholder="QUANTITY"
+              placeholderTextColor="gray"
+              value={productData.quantity}
+              onChangeText={text => handleInputChange('quantity', text)}
+              style={styles.inputField}
+            />
+            <TextInput
+              placeholder="STATUS"
+              placeholderTextColor="gray"
+              value={productData.status}
+              onChangeText={text => handleInputChange('status', text)}
+              style={styles.inputField}
+            />
+            {/* Add similar TextInputs for other fields */}
+            <Button text="Submit" onPress={handleSubmit} />
+            <Button text="Close" onPress={onClose} />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -57,5 +166,10 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'flex-end',
+
   },
+  inputField: { color: 'black', borderWidth: 1, marginVertical: 4 },
+  button: {
+    color: 'white',
+  }
 });
